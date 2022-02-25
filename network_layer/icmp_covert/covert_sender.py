5f89abaea5b39edf6dfa795149aa6e62eef76b49
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-02-25 21:05:20 +0100

    Update covert_sender.py

--- network_layer/icmp_covert/covert_sender.py
+++ network_layer/icmp_covert/covert_sender.py
@@ -4,14 +4,15 @@ from cryptography.fernet import Fernet
 import base64
 
 if __name__ == "__main__":
+	# Store arguments in variables
         ip_receiver = str(sys.argv[1])
         payload = str(sys.argv[2])
 
+	# Use hardcoded string as key for encryption of the message
         hardcoded_key = "xThisxisxaxsharedxhardcodedxkeyx".encode("ascii")
         covert_key = base64.urlsafe_b64encode(hardcoded_key)
         fernet = Fernet(covert_key)
 
+	# Encrypt message and send
         message =  fernet.encrypt(str.encode(payload))
-        send(IP(dst=ip_receiver) / ICMP(type=0) / message, verbose=False)
-
-
+        send(IP(dst=ip_receiver) / ICMP(type=1) / message, verbose=False)

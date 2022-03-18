4caf9000fe88cf9faf32dd3076b0f9a14b75663e
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2022-03-18 21:09:00 +0100

    Update SMTP-with-lib.py

--- application_layer/SMTP-with-lib.py
+++ application_layer/SMTP-with-lib.py
@@ -2,6 +2,17 @@ import sys
 import smtplib
 from email.message import EmailMessage
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.audio import MIMEAudio
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
+import mimetypes
+import email
+
+# python3 SMTPold.py "ahmetvanderbergg@gmail.com" "Raditya-05" "ahmetvanderbergg@gmail.com" "test.txt" "Hello"
+
 if __name__ == "__main__":
     sender_mail = str(sys.argv[1])
     sender_password = str(sys.argv[2])
@@ -9,14 +20,34 @@ if __name__ == "__main__":
     attachment = str(sys.argv[4])
     body = str(sys.argv[5])
 
-    msg = EmailMessage()
-    msg['From'] = sender_mail
-    msg['To'] = receiver_mail
-    msg['Subject'] = "New Email"
-    msg.set_content(body)
-    msg.add_attachment(attachment)
+    # msg = EmailMessage()
+    # msg['From'] = sender_mail
+    # msg['To'] = receiver_mail
+    # msg['Subject'] = "New Email"
+    # msg.set_content(body)
+    # msg.add_attachment(attachment)
 
-    mail_text = "From: {}\nTo: {}\nSubject: New Email\nBody: {}\nAttachment: {}".format(sender_mail, receiver_mail, body, attachment)
+    sender_address = sender_mail
+    sender_pass = sender_password
+    receiver_address = receiver_mail
+    mail_content = body
+
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'New Email'
+
+    message.attach(MIMEText(mail_content, 'plain'))
+    attach_file_name = attachment
+    attach_file = open(attach_file_name, 'rb')  # Open the file as binary mode
+    payload = MIMEBase('application', 'octet-stream')
+    payload.set_payload((attach_file).read())
+    encoders.encode_base64(payload)  # encode the attachment
+    attach_file.close()
+    payload.add_header('Content-Disposition', 'attachment', filename=attach_file_name)
+    message.attach(payload)
+
+    # mail_text = "From: {}\nTo: {}\nSubject: New Email\nBody: {}\nAttachment: {}".format(sender_mail, receiver_mail, body, attachment)
 
     try:
         # server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -25,8 +56,8 @@ if __name__ == "__main__":
         server.ehlo()
         server.starttls()
         server.login(sender_mail, sender_password)
-        server.send_message(msg)
+        server.sendmail(sender_mail, receiver_mail, message.as_string())
         server.close()
-        print(mail_text)
+        print(message.as_string())
     except:
         pass 
